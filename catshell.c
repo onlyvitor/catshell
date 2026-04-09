7bb063b93e9c5e814feb198cfc5fa341e9dca9a6
vito <vitorgn1franca@protonmail.com>
2026-04-09 11:57:00 -0300

    refactor: make code more beauaty(?)

--- catshell.c
+++ catshell.c
@@ -4,16 +4,16 @@
 
 int main(int ac, char **av) {
     char *line;
+    char **args;
+
     //REPL (read, evaluate, print, loop)
     while(MAGIC_NUMBER){
         // read
+        printf("%s", line);
         line = cat_read_line();
         if (line == NULL) {
             break;  // EOF (CTRL+D) pressed
         }
-        printf(">%s", line);
-        pause();
-
     }
     return EXIT_SUCCESS;
 }
--- readline/cat_read_line.c
+++ readline/cat_read_line.c
@@ -3,17 +3,22 @@
 char *cat_read_line(void) {
     char *buf;
     size_t buf_size;
+    int position;
+
+    char **args;
+
+    printf("~$ ");
 
     buf = NULL;
     if (getline(&buf, &buf_size, stdin) == -1) {
         buf = NULL;
+
         if (feof(stdin)) {
             printf(ANSI_COLOR_BLUE"[EOF]"RESET);
         }
         else{
             printf(ANSI_COLOR_BLUE" a Error ocurred\n"RESET);
         }
-        printf("( %s)", buf);//debug
     }
     return buf;
 }
